2cca2f4e645bec7e2c3ea7049d2b876b4132e977
jmanzelm <jmanzelm@stevens.edu>
2018-03-05 13:27:51 -0500

    fcfs and rr functions

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -9,18 +9,33 @@
 #include <pthread.h>
 #include <random>
 #include <sys/time.h>
+#include <unistd.h>
 
 using namespace std;
 
 typedef struct {
 	int id;
-	int time;
+	timeval time;
 	int life;
 } product;
 
-int size;
+typedef struct {
+	int num;
+} fcfs_args;
+
+typedef struct {
+	int quantum;
+	int num;
+} rr_args;
+
 int max_products;
-queue<product*> Q = new queue<product*>;
+int prods_made = 0;
+int prods_used = 0;
+int size;
+queue<product*> Q;
+
+pthread_mutex_t permissions;
+pthread_cond_t cond;
 
 product* create_product() {
 	product* new_product = new product;
@@ -33,123 +48,260 @@ inline void delete_product(product* prod) {
 	delete prod;
 }
 
-void first_come_first_serve_P(int seed) {
-
+int fibbo(int num) {
+	if (num < 2) {
+		return 1;
+	}
+	return fibbo(num-1) + fibbo(num-2);
 }
 
-void first_come_first_serve_C(int seed) {
+void* first_come_first_serve_P(void* arguments) {
+	fcfs_args* args = (fcfs_args*)arguments;
+	pthread_mutex_lock(&permissions);
+	while(1) {
+		if (prods_made == max_products) {
+			break;
+		}
+		if(Q.size() == size) {
+			pthread_cond_signal(&cond);
+			pthread_cond_wait(&cond, &permissions);
+			continue;
+		}
 
+		product* p = create_product();
+		cout << "Producer " << args->num << " has produced product " << p->id << endl;
+		Q.push(p);
+		++prods_made;
+
+		pthread_mutex_unlock(&permissions);
+		usleep(100000);
+		pthread_mutex_lock(&permissions);
+	}
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&permissions);
 }
 
-void round_robin_P(int quantum, int seed) {
+void* first_come_first_serve_C(void* arguments) {
+	fcfs_args* args = (fcfs_args*)arguments;
+	pthread_mutex_lock(&permissions);
+	while(1) {
+		if (prods_used == max_products) {
+			break;
+		}
+		if(Q.empty()) {
+			pthread_cond_signal(&cond);
+			pthread_cond_wait(&cond, &permissions);
+			continue;
+		}
 
+		product* p = Q.front();
+		Q.pop();
+		++prods_used;
+
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&permissions);
+
+		for (int i = 0; i<p->life; ++i) {
+			fibbo(10);
+		}
+		cout << "Consumer " << args->num << " has consumed product " << p->id << endl;
+		delete_product(p);
+		usleep(100000);
+		pthread_mutex_lock(&permissions);
+	}
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&permissions);
 }
 
-void round_robin_C(int quantum, int seed) {
+void* round_robin_P(void* arguments) {
+	rr_args* args = (rr_args*)arguments;
+	pthread_mutex_lock(&permissions);
+	while(1) {
+		if (prods_made == max_products) {
+			break;
+		}
+		if(Q.size() == size) {
+			pthread_cond_signal(&cond);
+			pthread_cond_wait(&cond, &permissions);
+			continue;
+		}
 
+		product* p = create_product();
+		cout << "Producer " << args->num << " has produced product " << p->id << endl;
+		Q.push(p);
+		++prods_made;
+
+		pthread_mutex_unlock(&permissions);
+		usleep(100000);
+		pthread_mutex_lock(&permissions);
+	}
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&permissions);
+}
+
+void* round_robin_C(void* arguments) {
+	rr_args* args = (rr_args*)arguments;
+	pthread_mutex_lock(&permissions);
+	while(1) {
+		if (prods_used == max_products) {
+			break;
+		}
+		if(Q.empty()) {
+			pthread_cond_signal(&cond);
+			pthread_cond_wait(&cond, &permissions);
+			continue;
+		}
+
+		product* p = Q.front();
+		Q.pop();
+
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&permissions);
+		if (p->life <= args->quantum) {
+			for (int i = 0; i<p->life; ++i) {
+				fibbo(10);
+			}
+			++prods_used;
+			cout << "Consumer " << args->num << " has consumed product " << p->id << endl;
+			delete_product(p);
+			usleep(100000);
+			pthread_mutex_lock(&permissions);
+		}
+		else {
+			for (int i = 0; i<args->quantum; ++i) {
+				fibbo(10);
+			}
+			p->life -= args->quantum;
+			usleep(100000);
+			pthread_mutex_lock(&permissions);
+			Q.push(p);
+		}
+	}
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&permissions);
 }
 
 int main(int argc, char* argv[]) {
-	int num_producers, num_consumers, num_products, Qsize, Atype, quantum, seed;
+	int num_producers, num_consumers, num_products, Qsize, Atype, quantum;
+	unsigned int seed;
 	istringstream iss;
 
 	if (argc != 8) {
 		cerr << "Usage: ./assign1 <producers> <consumers> <products> <Q size> <algo type> <quantum> <seed>" << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
 
 	iss.str(argv[1]);
 	if (!(iss >> num_producers) || num_producers < 1) {
 		cerr << "Error: Invalid number of producers '" << argv[1] << "'." << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
 
 	iss.str(argv[2]);
 	if (!(iss >> num_consumers) || num_consumers < 1) {
 		cerr << "Error: Invalid number of consumers '" << argv[2] << "'." << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
 
 	iss.str(argv[3]);
 	if (!(iss >> num_products) || num_products < 1) {
 		cerr << "Error: Invalid number of products '" << argv[3] << "'." << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
 
 	iss.str(argv[4]);
 	if (!(iss >> Qsize) || Qsize < 1) {
 		cerr << "Error: Invalid queue size '" << argv[4] << "'." << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
 
 	iss.str(argv[5]);
 	if (!(iss >> Atype) || Atype < 0 || Atype > 1) {
 		cerr << "Error: Invalid algorithm type '" << argv[5] << "'." << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
 
 	iss.str(argv[6]);
 	if (!(iss >> quantum) || quantum < 1) {
 		cerr << "Error: Invalid number of producers '" << argv[6] << "'." << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
 
 	iss.str(argv[7]);
 	if (!(iss >> seed) || seed < 1) {
 		cerr << "Error: Invalid number of producers '" << argv[7] << "'." << endl;
-		pthread_exit(1);
+		return 1;
 	}
 	iss.clear();
-<<<<<<< HEAD
 
 	size = Qsize;
 	max_products = num_products;
+	srandom(seed);
 
-=======
 	pthread_t producer_thread[num_producers];
 	pthread_t consumer_thread[num_consumers];
 	int np[num_producers];
 	int nc[num_consumers];
->>>>>>> dd1ebb2b68835d79235cf0610f4c512053e3e3f8
-	if (Atype) {
-		for (i=0;i<num_producers;i++){
-    			np[i] = i;
-    			pthread_create(&producer_thread[i],NULL,round_robinP,&np[i]);
-  		}
-  		for (i=0;i<num_producers;i++) 
-    			pthread_join(producer_thread[i],NULL);
 
-		for (i=0;i<num_consumers;i++){
-    			nc[i] = i;
-    			pthread_create(&consumer_thread[i],NULL,round_robinC,&nc[i]);
+	if (Atype) {
+		pthread_mutex_init(&permissions, NULL);
+		pthread_cond_init(&cond, NULL);
+		for (int i=0;i<num_producers;i++){
+			np[i] = i;
+			rr_args temp;
+			temp.quantum = quantum;
+			temp.num = i;
+			pthread_create(&producer_thread[i],NULL,round_robin_P,(void*)&temp);
   		}
-  		for (i=0;i<num_consumers;i++)
-    			pthread_join(producer_thread[i],NULL);
-		
-		pthread_exit(0);
+
+		for (int i=0;i<num_consumers;i++){
+			nc[i] = i;
+			rr_args temp;
+			temp.quantum = quantum;
+			temp.num = i;
+			pthread_create(&consumer_thread[i],NULL,round_robin_C,(void*)&temp);
+  		}
+
+  		for (int i=0;i<num_producers;i++) 
+    		pthread_join(producer_thread[i],NULL);
+
+  		for (int i=0;i<num_consumers;i++)
+    		pthread_join(consumer_thread[i],NULL);
+		pthread_mutex_destroy(&permissions);
+		pthread_cond_destroy(&cond);
+		return 0;
 	}
 
-		for (i=0;i<num_producers;i++){
-    			np[i] = i;
-    			pthread_create(&producer_thread[i],NULL,first_come_first_serveP,&np[i]);
-  		}
-  		for (i=0;i<num_producers;i++) 
-    			pthread_join(producer_thread[i],NULL);
+	pthread_mutex_init(&permissions, NULL);
+	pthread_cond_init(&cond, NULL);
+	for (int i=0;i<num_producers;i++){
+		np[i] = i;
+		fcfs_args temp;
+		temp.num = i;
+		pthread_create(&producer_thread[i],NULL,first_come_first_serve_P,(void*)&temp);
+	}
 
-		for (i=0;i<num_consumers;i++){
-    			nc[i] = i;
-    			pthread_create(&consumer_thread[i],NULL,first_come_first_serveC,&nc[i]);
-  		}
-  		for (i=0;i<num_consumers;i++)
-    			pthread_join(producer_thread[i],NULL);
+	for (int i=0;i<num_consumers;i++){
+		nc[i] = i;
+		fcfs_args temp;
+		temp.num = i;
+		pthread_create(&consumer_thread[i],NULL,first_come_first_serve_C,(void*)&temp);
+	}
 
-	pthread_exit(0);
+	for (int i=0;i<num_producers;i++) 
+		pthread_join(producer_thread[i],NULL);
+
+	for (int i=0;i<num_consumers;i++)
+		pthread_join(consumer_thread[i],NULL);
+	pthread_mutex_destroy(&permissions);
+	pthread_cond_destroy(&cond);
+	return 0;
 }
 
