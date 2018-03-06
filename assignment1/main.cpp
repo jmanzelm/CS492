c7f3b1438e1f50fac702f3f8fe89f2abbb0d3a95
jmanzelm <jmanzelm@stevens.edu>
2018-03-05 22:56:11 -0500

    added timing functionalities

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -11,14 +11,15 @@
 #include <time.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <vector>
+#include <limits>
 
 using namespace std;
 
 typedef struct {
 	int id;
-	timeval time;
-	timeval wait;
 	int life;
+	double wait;
 	double t1;
 	double t3;
 } product;
@@ -37,6 +38,8 @@ int prods_made = 0;
 int prods_used = 0;
 int size;
 queue<product*> Q;
+vector<double> turn_around;
+vector<double> wait;
 
 pthread_mutex_t permissions;
 pthread_cond_t cond;
@@ -44,8 +47,9 @@ pthread_cond_t cond;
 product* create_product() {
 	product* new_product = new product;
 	new_product->id = random();
-	gettimeofday(&(new_product->time), NULL);
-	new_product->t1 = (new_product->time).tv_sec+((new_product->time).tv_usec/1000000.0);
+	timeval temp;
+	gettimeofday(&temp, NULL);
+	new_product->t1 = temp.tv_sec+(temp.tv_usec/1000000.0);
 	new_product->life = random() % 1024;
 }
 
@@ -74,9 +78,10 @@ void* first_come_first_serve_P(void* arguments) {
 		}
 
 		product* p = create_product();
+		timeval temp;
+		gettimeofday(&temp, NULL);
+		p->t3 = temp.tv_sec+(temp.tv_usec/1000000.0);
 		Q.push(p);
-		gettimeofday(&(p->wait), NULL);
-		p->t3 = (p->wait).tv_sec+((p->wait).tv_usec/1000000.0);	
 		cout << "Producer " << args->num << " has produced product " << p->id << endl;
 		++prods_made;
 
@@ -103,18 +108,15 @@ void* first_come_first_serve_C(void* arguments) {
 
 		product* p = Q.front();
 		Q.pop();
-		gettimeofday(&(p->wait), NULL);
-		double t4=(p->wait).tv_sec+((p->time).tv_usec/1000000.0);
-		double t;
-		t = t4 - (p->t3);
-		cout << "Product " << p->id << " was waiting for " << t << " seconds." << endl;
+
+		timeval temp;
+		gettimeofday(&temp, NULL);
+		double t4 = temp.tv_sec+(temp.tv_usec/1000000.0);
+		double t = t4 - (p->t3);
+		wait.push_back(t);
+
 		++prods_used;
 		cout << "Consumer " << args->num << " has consumed product " << p->id << endl;
-		gettimeofday(&(p->time), NULL);
-		double t2=(p->time).tv_sec+((p->time).tv_usec/1000000.0);
-		double t0;
-		t0 = t2 - (p->t1);
-		cout << "Product " << p->id << " was processed for " << t0 << " seconds." << endl;
 		pthread_cond_signal(&cond);
 		pthread_mutex_unlock(&permissions);
 
@@ -122,9 +124,14 @@ void* first_come_first_serve_C(void* arguments) {
 			fibbo(10);
 		}
 
+		gettimeofday(&temp, NULL);
+		double t2 = temp.tv_sec+(temp.tv_usec/1000000.0);
+		double t0 = t2 - (p->t1);
+
 		delete_product(p);
 		usleep(100000);
 		pthread_mutex_lock(&permissions);
+		turn_around.push_back(t0);
 	}
 	pthread_cond_signal(&cond);
 	pthread_mutex_unlock(&permissions);
@@ -144,11 +151,11 @@ void* round_robin_P(void* arguments) {
 		}
 
 		product* p = create_product();
+		timeval temp;
+		gettimeofday(&temp, NULL);
+		p->t3 = temp.tv_sec+(temp.tv_usec/1000000.0);
 		Q.push(p);
-		gettimeofday(&(p->wait), NULL);
-		p->t3 = (p->wait).tv_sec+((p->wait).tv_usec/1000000.0);	
 		cout << "Producer " << args->num << " has produced product " << p->id << endl;
-		++prods_made;
 
 		pthread_mutex_unlock(&permissions);
 		usleep(100000);
@@ -173,28 +180,31 @@ void* round_robin_C(void* arguments) {
 
 		product* p = Q.front();
 		Q.pop();
-		gettimeofday(&(p->wait), NULL);
-		double t4=(p->wait).tv_sec+((p->time).tv_usec/1000000.0);
-		double t;
-		t = t4 - (p->t3);
-		cout << "Product " << p->id << " was waiting for " << t << " seconds." << endl;
+
+		timeval temp;
+		gettimeofday(&temp, NULL);
+		double t4 = temp.tv_sec+(temp.tv_usec/1000000.0);
+		double t = t4 - (p->t3);
+		p->wait += t;
+
 		pthread_cond_signal(&cond);
 		if (p->life <= args->quantum) {
+			wait.push_back(p->wait);
 			cout << "Consumer " << args->num << " has consumed product " << p->id << endl;
 			pthread_mutex_unlock(&permissions);
 			for (int i = 0; i<p->life; ++i) {
 				fibbo(10);
 			}
 			++prods_used;
-			cout << "Consumer " << args->num << " has consumed product " << p->id << endl;
-			gettimeofday(&(p->time), NULL);
-			double t2=(p->time).tv_sec+((p->time).tv_usec/1000000.0);
-			double t0;
-			t0 = t2 - (p->t1);
-			cout << "Product " << p->id << " was processed for " << t0 << " seconds." << endl;
+
+			gettimeofday(&temp, NULL);
+			double t2 = temp.tv_sec+(temp.tv_usec/1000000.0);
+			double t0 = t2 - (p->t1);
+
 			delete_product(p);
 			usleep(100000);
 			pthread_mutex_lock(&permissions);
+			turn_around.push_back(t0);
 		}
 		else {
 			pthread_mutex_unlock(&permissions);
@@ -285,6 +295,11 @@ int main(int argc, char* argv[]) {
 		rr_args* c_args[num_consumers];
 		pthread_mutex_init(&permissions, NULL);
 		pthread_cond_init(&cond, NULL);
+
+		timeval total_tv;
+		gettimeofday(&total_tv, NULL);
+		double total_start = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
+
 		for (int i=0;i<num_producers;i++){
 			np[i] = i;
 			rr_args* temp = new rr_args;
@@ -293,6 +308,9 @@ int main(int argc, char* argv[]) {
 			p_args[i] = temp;
 			pthread_create(&producer_thread[i],NULL,round_robin_P,(void*)temp);
   		}
+
+		gettimeofday(&total_tv, NULL);
+		double consumer_start = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
 
 		for (int i=0;i<num_consumers;i++){
 			nc[i] = i;
@@ -306,8 +324,14 @@ int main(int argc, char* argv[]) {
   		for (int i=0;i<num_producers;i++) 
     		pthread_join(producer_thread[i],NULL);
 
+		gettimeofday(&total_tv, NULL);
+		double producer_end = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
+
   		for (int i=0;i<num_consumers;i++)
     		pthread_join(consumer_thread[i],NULL);
+
+		gettimeofday(&total_tv, NULL);
+		double total_end = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
 
 		pthread_mutex_destroy(&permissions);
 		pthread_cond_destroy(&cond);
@@ -317,6 +341,41 @@ int main(int argc, char* argv[]) {
 		for (int i=0; i<num_consumers; ++i) {
 			delete c_args[i];
 		}
+
+		double t_min = numeric_limits<double>::infinity(), t_max = 0, t_average = 0;
+		for (int i=0; i<turn_around.size(); ++i) {
+			if (turn_around.at(i) < t_min) {
+				t_min = turn_around.at(i);
+			}
+
+			if (turn_around.at(i) > t_max) {
+				t_max = turn_around.at(i);
+			}
+
+			t_average += turn_around.at(i);
+		}
+		t_average /= num_products;
+
+		double w_min = numeric_limits<double>::infinity(), w_max = 0, w_average = 0;
+		for (int i=0; i<wait.size(); ++i) {
+			if (wait.at(i) < w_min) {
+				w_min = wait.at(i);
+			}
+
+			if (wait.at(i) > w_max) {
+				w_max = wait.at(i);
+			}
+
+			w_average += wait.at(i);
+		}
+		w_average /= num_products;
+
+		cout << "Total processing time: " << total_end-total_start << "s" << endl;
+		cout << "Producer throughput: " << (num_products/(producer_end-total_start))* 60 << "prods/m" << endl;
+		cout << "Consumer throughput: " << (num_products/(total_end-consumer_start))* 60 << "prods/m" << endl;
+		cout << "Turn-around Min: " << t_min << ", Max: " << t_max << ", Average: " << t_average << endl;
+		cout << "Wait Min: " << w_min << ", Max: " << w_max << ", Average: " << w_average << endl;
+
 		return 0;
 	}
 
@@ -324,6 +383,11 @@ int main(int argc, char* argv[]) {
 	fcfs_args* c_args[num_consumers];
 	pthread_mutex_init(&permissions, NULL);
 	pthread_cond_init(&cond, NULL);
+
+	timeval total_tv;
+	gettimeofday(&total_tv, NULL);
+	double total_start = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
+
 	for (int i=0;i<num_producers;i++){
 		np[i] = i;
 		fcfs_args* temp = new fcfs_args;
@@ -331,6 +395,9 @@ int main(int argc, char* argv[]) {
 		p_args[i] = temp;
 		pthread_create(&producer_thread[i],NULL,first_come_first_serve_P,(void*)temp);
 	}
+
+	gettimeofday(&total_tv, NULL);
+	double consumer_start = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
 
 	for (int i=0;i<num_consumers;i++){
 		nc[i] = i;
@@ -343,8 +410,14 @@ int main(int argc, char* argv[]) {
 	for (int i=0;i<num_producers;i++) 
 		pthread_join(producer_thread[i],NULL);
 
+	gettimeofday(&total_tv, NULL);
+	double producer_end = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
+
 	for (int i=0;i<num_consumers;i++)
 		pthread_join(consumer_thread[i],NULL);
+
+	gettimeofday(&total_tv, NULL);
+	double total_end = total_tv.tv_sec+(total_tv.tv_usec/1000000.0);
 
 	pthread_mutex_destroy(&permissions);
 	pthread_cond_destroy(&cond);
@@ -354,6 +427,41 @@ int main(int argc, char* argv[]) {
 	for (int i=0; i<num_consumers; ++i) {
 		delete c_args[i];
 	}
+
+	double t_min = numeric_limits<double>::infinity(), t_max = 0, t_average = 0;
+	for (int i=0; i<turn_around.size(); ++i) {
+		if (turn_around.at(i) < t_min) {
+			t_min = turn_around.at(i);
+		}
+
+		if (turn_around.at(i) > t_max) {
+			t_max = turn_around.at(i);
+		}
+
+		t_average += turn_around.at(i);
+	}
+	t_average /= num_products;
+
+	double w_min = numeric_limits<double>::infinity(), w_max = 0, w_average = 0;
+	for (int i=0; i<wait.size(); ++i) {
+		if (wait.at(i) < w_min) {
+			w_min = wait.at(i);
+		}
+
+		if (wait.at(i) > w_max) {
+			w_max = wait.at(i);
+		}
+
+		w_average += wait.at(i);
+	}
+	w_average /= num_products;
+
+	cout << "Total processing time: " << total_end-total_start << "s" << endl;
+	cout << "Producer throughput: " << (num_producers/(producer_end-total_start))* 60 << "prods/m" << endl;
+	cout << "Consumer throughput: " << (num_products/(total_end-consumer_start))* 60 << "prods/m" << endl;
+	cout << "Turn-around Min: " << t_min << ", Max: " << t_max << ", Average: " << t_average << endl;
+	cout << "Wait Min: " << w_min << ", Max: " << w_max << ", Average: " << w_average << endl;
+
 	return 0;
 }
 
